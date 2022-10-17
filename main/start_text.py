a0138949ebc18abc640610ee38177698ce64b98d
Rohit <114798267+Rajrohit000666@users.noreply.github.com>
2022-10-18 01:17:15 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -19,7 +19,7 @@ async def start_cmd(bot, msg):
 
 @Client.on_callback_query(filters.regex("start"))
 async def start(bot, msg, cb=True):   
-    txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://github.com/MrMKN>MrMKN</a></b>"                                     
+    txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://github.com/Rajrohit000666>𝗖𝘂𝘁𝗲 𝗴𝗶𝗿𝗹</a></b>"                                     
     button= [[
         InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/movie4ubotschannel")
         ],[
